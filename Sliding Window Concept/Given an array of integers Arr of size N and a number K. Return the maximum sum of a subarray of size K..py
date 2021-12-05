9d76e0ade36ff1058e59038e0a342cc185ef97f3
Tejpal kumawat <65027063+tejpal123456789@users.noreply.github.com>
2021-12-06 01:39:02 +0530

    Create  Given an array of integers Arr of size N and a number K. Return the maximum sum of a subarray of size K..py

---
+++ Sliding Window Concept/Given an array of integers Arr of size N and a number K. Return the maximum sum of a subarray of size K..py
@@ -0,0 +1,25 @@
+
+arr = [100, 200, 300, 400]
+k=2
+x=float('-inf')
+def max_sum_subarray(arr,k):
+ j=0
+ i=0
+ sum=0
+ while j<len(arr):
+    sum=sum+arr[j]      # summing elements of array
+    if j-i+1<k:
+         j=j+1          # increment of box
+
+    elif j-i+1==k:
+        
+        mx=max(sum,x)         # finding max sum by previous sum of same sliding window
+         
+        sum=mx-arr[i]
+
+        j=j+1
+        i=i+1
+
+ return mx
+        
+print(max_sum_subarray(arr,k))
